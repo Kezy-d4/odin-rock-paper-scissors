434b4fd040dd23bef2703eecd2164308f2be401f
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-10-25 17:29:33 -0400

    Write getComputerChoice()
    Each hand: Rock, Paper, and Scissors has a 1/3
    chance of being chosen by the computer based on
    this function.

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+function getComputerChoice() {
+  randomNumber = Math.random();
+  console.log(randomNumber);
+
+  if (randomNumber < 0.33) {
+    console.log("Rock");
+  } else if (randomNumber >= 0.33 && randomNumber <= 0.66) {
+    console.log("Paper");
+  } else {
+    console.log("Scissors");
+  }
+}
+
+getComputerChoice();
