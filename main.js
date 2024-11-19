0dfb3604cab126ee36897f5d43bb42fae807f1fa
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-11-18 19:44:29 -0500

    Update instructions, styling, getComputerChoice()

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 function getComputerChoice() {
-  let randomNumber = Math.random() * 100;
+  let randomNumber = Math.floor(Math.random() * 3);
   let computerChoice;
 
-  if (randomNumber < 33.33) {
+  if (randomNumber === 0) {
     computerChoice = "Rock";
-  } else if (randomNumber >= 33.33 && randomNumber <= 66.66) {
+  } else if (randomNumber === 1) {
     computerChoice = "Paper";
-  } else {
+  } else if (randomNumber === 2) {
     computerChoice = "Scissors";
   }
 
   console.log(randomNumber);
+  console.log(computerChoice);
   return computerChoice;
 }
 
+getComputerChoice();
+
+/*
 function getHumanChoice() {
   // let humanChoice = prompt("Which hand will you throw: Rock, Paper, or Scissors? Type in your choice.");
 
@@ -24,9 +28,11 @@ function getHumanChoice() {
 
     button.addEventListener('click', () => {
       humanChoice = button.className;
-      console.log(humanChoice);
+      // console.log(humanChoice);
+
+      playRound(humanChoice, getComputerChoice());
       
-      return humanChoice;
+      
     });
   });
 
@@ -43,80 +49,81 @@ function getHumanChoice() {
   // }
 }
 
-getHumanChoice();
+// getHumanChoice();
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+// function playGame() {
+//   let humanScore = 0;
+//   let computerScore = 0;
 
-  function playRound(humanChoice, computerChoice) {
+  // function playRound(humanChoice, computerChoice) {
+
     // if (humanChoice !== undefined) {
-    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.substring(1);
+    // humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.substring(1);
     // }
 
     // if (humanChoice === undefined) {
     //   console.warn("You failed to input a valid hand entry. This match is invalidated. Please reset the page to play again.");
     // } else {
-    console.log(`You throw ${humanChoice}. Computer throws ${computerChoice}.`);
+    // console.log(`You throw ${humanChoice}. Computer throws ${computerChoice}.`);
     // }
 
-    if (humanChoice === computerChoice) {
-      console.log("This hand is a draw! No points are scored.");
+    // if (humanChoice === computerChoice) {
+    //   console.log("This hand is a draw! No points are scored.");
 
-    } else if (humanChoice === "Rock") {
-        if (computerChoice === "Scissors") {
-          humanScore++;
-          console.log("You win this hand! Rock beats Scissors.");
-        } else if (computerChoice === "Paper") {
-          computerScore++;
-          console.log("Computer wins this hand! Paper beats Rock.");
-        }
+    // } else if (humanChoice === "Rock") {
+    //     if (computerChoice === "Scissors") {
+    //       humanScore++;
+    //       console.log("You win this hand! Rock beats Scissors.");
+    //     } else if (computerChoice === "Paper") {
+    //       computerScore++;
+    //       console.log("Computer wins this hand! Paper beats Rock.");
+    //     }
 
-    } else if (humanChoice === "Paper") {
-       if (computerChoice === "Rock") {
-        humanScore++;
-        console.log("You win this hand! Paper beats Rock.");
-       } else if (computerChoice === "Scissors") {
-        computerScore++;
-        console.log("Computer wins this hand! Scissors beats Paper.");
-       }
+    // } else if (humanChoice === "Paper") {
+    //    if (computerChoice === "Rock") {
+    //     humanScore++;
+    //     console.log("You win this hand! Paper beats Rock.");
+    //    } else if (computerChoice === "Scissors") {
+    //     computerScore++;
+    //     console.log("Computer wins this hand! Scissors beats Paper.");
+    //    }
 
-    } else if (humanChoice === "Scissors") {
-        if (computerChoice === "Paper") {
-          humanScore++;
-          console.log("You win this hand! Scissors beats Paper");
-        } else if (computerChoice === "Rock") {
-          computerScore++;
-          console.log("Computer wins this hand! Rock beats Scissors.");
-        }
-    }
+    // } else if (humanChoice === "Scissors") {
+    //     if (computerChoice === "Paper") {
+    //       humanScore++;
+    //       console.log("You win this hand! Scissors beats Paper");
+    //     } else if (computerChoice === "Rock") {
+    //       computerScore++;
+    //       console.log("Computer wins this hand! Rock beats Scissors.");
+    //     }
+    // }
 
     // if (humanChoice !== undefined) {
-      console.log(`Player score: ${humanScore}`);
-      console.log(`Computer score: ${computerScore}`);
+      // console.log(`Player score: ${humanScore}`);
+      // console.log(`Computer score: ${computerScore}`);
     // }
-  }
+  // }
 
-  console.log("Round1:");
-  playRound(getHumanChoice(), getComputerChoice());
-  console.log("Round2:");
-  playRound(getHumanChoice(), getComputerChoice());
-  console.log("Round3:");
-  playRound(getHumanChoice(), getComputerChoice());
-  console.log("Round4:");
-  playRound(getHumanChoice(), getComputerChoice());
-  console.log("Round5:");
-  playRound(getHumanChoice(), getComputerChoice());
+  // console.log("Round1:");
+  // playRound(getHumanChoice(), getComputerChoice());
+  // console.log("Round2:");
+  // playRound(getHumanChoice(), getComputerChoice());
+  // console.log("Round3:");
+  // playRound(getHumanChoice(), getComputerChoice());
+  // console.log("Round4:");
+  // playRound(getHumanChoice(), getComputerChoice());
+  // console.log("Round5:");
+  // playRound(getHumanChoice(), getComputerChoice());
   
-  if (humanScore === computerScore) {
-    console.log("This match is a draw! Good game.");
-  } else if (humanScore > computerScore) {
-    console.log("You win the match! Well played.");
-  } else {
-    console.log("Computer wins the match! Good game.");
-  }
-}
+  // if (humanScore === computerScore) {
+  //   console.log("This match is a draw! Good game.");
+  // } else if (humanScore > computerScore) {
+  //   console.log("You win the match! Well played.");
+  // } else {
+  //   console.log("Computer wins the match! Good game.");
+  // }
+// }
 
 // playGame();
 
-
+*/
